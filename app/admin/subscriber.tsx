973563d08e8d661f5450e705f7f045977d547973
Winfrida-Obinyo <obinyowinfridaa@gmail.com>
2024-09-27 09:41:10 +0300

    netlify build optimization

--- app/admin/subscriber.tsx
+++ app/admin/subscriber.tsx
@@ -1,34 +1,34 @@
-// pages/api/subscribe.ts
-import { PrismaClient } from '@prisma/client'
+// // pages/api/subscribe.ts
+// import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { email } = req.body
+// export default async function handler(req, res) {
+//   if (req.method === 'POST') {
+//     const { email } = req.body
 
-    // Check if email is provided
-    if (!email || !email.includes('@')) {
-      return res.status(400).json({ error: 'Invalid email address' })
-    }
+//     // Check if email is provided
+//     if (!email || !email.includes('@')) {
+//       return res.status(400).json({ error: 'Invalid email address' })
+//     }
 
-    try {
-      // Save the email in the database
-      const subscriber = await prisma.subscriber.create({
-        data: { email },
-      })
+//     try {
+//       // Save the email in the database
+//       const subscriber = await prisma.subscriber.create({
+//         data: { email },
+//       })
 
-      return res.status(200).json({ success: true, subscriber })
-    } catch (error) {
-      if (error.code === 'P2002') {
-        // Unique constraint error, email already exists
-        return res.status(400).json({ error: 'Email is already subscribed' })
-      }
-      // Handle other errors
-      return res.status(500).json({ error: 'Something went wrong' })
-    }
-  } else {
-    res.setHeader('Allow', ['POST'])
-    return res.status(405).end(`Method ${req.method} Not Allowed`)
-  }
-}
+//       return res.status(200).json({ success: true, subscriber })
+//     } catch (error) {
+//       if (error.code === 'P2002') {
+//         // Unique constraint error, email already exists
+//         return res.status(400).json({ error: 'Email is already subscribed' })
+//       }
+//       // Handle other errors
+//       return res.status(500).json({ error: 'Something went wrong' })
+//     }
+//   } else {
+//     res.setHeader('Allow', ['POST'])
+//     return res.status(405).end(`Method ${req.method} Not Allowed`)
+//   }
+// }
